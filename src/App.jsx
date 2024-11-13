dd40276ea003440f4c3c6018923edcc82e5959bc
Sara Guaiume <130059301+saraqwe123@users.noreply.github.com>
2024-11-13 11:57:25 -0300

    .

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import AddTodo from './components/AddTodo'
+import TodoList from './components/TodoList'
+
+function App() {
+
+  return (
+    <div className='w-screen h-screen'>
+      <AddTodo/>
+      <TodoList/>
+    </div>
+
+  )
+}
+
+export default App
---
+++ src/components/AddTodo.jsx
@@ -0,0 +1,12 @@
+function AddTodo() {
+    return(
+        <div>
+            <div>
+                <input value type="text" className="bg-sky-500" />
+                <button>Criar</button>
+            </div>
+        </div>
+    )
+}
+
+export default AddTodo
